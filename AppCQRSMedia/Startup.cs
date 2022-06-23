4087f05d84e5e88ba749036bba105d4fefe35490
AlfredRdz <alfredo12_1998@hotmail.com>
2022-06-23 09:23:34 -0500

    a

--- AppCQRSMedia/Startup.cs
+++ AppCQRSMedia/Startup.cs
@@ -1,17 +1,26 @@
+using AppCQRS;
+using AppCQRSMedia.Commands.AddPersona;
 using AppCQRSMedia.CQRS.Application.Queries;
 using AppCQRSMedia.Domain.AggregatesModel.PersonaAggregate;
 using AppCQRSMedia.Infrastructure;
 using AppCQRSMedia.Infrastructure.Repositories;
+using AppCQRSMedia.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace AppCQRSMedia
 {
