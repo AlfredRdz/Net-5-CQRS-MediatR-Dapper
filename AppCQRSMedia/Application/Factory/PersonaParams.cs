ed72150209c12eb818745a1386c2a21f2f8da926
AlfredRdz <alfredo12_1998@hotmail.com>
2022-06-23 09:14:33 -0500

    cambios

---
+++ AppCQRSMedia/Application/Factory/PersonaParams.cs
@@ -0,0 +1,10 @@
+﻿namespace AppCQRSMedia.CQRS.Application.Factory
+{
+    public class PersonaParams
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? Age { get; set; }
+    }
+}
